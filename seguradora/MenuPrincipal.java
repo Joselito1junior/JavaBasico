1b9268c1282581c405d9eb0574152d246ff1559f
Joselito Junior <junior-joselito@hotmail.com>
2017-10-04 08:49:12 -0300

    Criação do Menu Principal

---
+++ seguradora/MenuPrincipal.java
@@ -0,0 +1,18 @@
+package br.unipe.java.seguradora;
+
+public class MenuPrincipal {
+	void exibeMenu()
+	{
+		System.out.println("[MENU PRINCIPAL]");
+		
+		System.out.println("1 - CADASTRO DE CLIENTES");
+		System.out.println("2 - CADASTRO DE CONTRATOS");
+		System.out.println("3 - LISTA DE CLIENTES");
+		System.out.println("4 - LISTA DE CONTRATOS");
+		System.out.println("5 - CADASTRO DE SINISTRO");
+		System.out.println("6 - LISTA DE SINISTRO");
+		System.out.println("7 - LISTA DE CONTRATOS SEM SINISTRO");
+		System.out.println("8 - SAIR ");
+	}
+
+}
---
+++ seguradora/UsaMenu.java
@@ -0,0 +1,27 @@
+package br.unipe.java.seguradora;
+
+import java.util.Scanner;
+
+public class UsaMenu {
+	public static void main(String[] args) {
+		
+		int opcao;
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		MenuPrincipal menu = new MenuPrincipal();
+		
+		do
+		{
+			System.out.println("----------------------");
+			System.out.println("SEGURADORA SANTA LUZIA");
+			System.out.println("----------------------");
+			menu.exibeMenu();
+			
+			System.out.print("Escolha uma opção: ");
+			opcao = leitor.nextInt();
+			
+		}while(opcao != 8);
+		System.out.println("Você Saiu! Até mais.");
+	}
+}
